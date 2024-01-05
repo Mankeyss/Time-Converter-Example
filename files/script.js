b7cfe2f7bcfbe6b1e8ee1dbbff5419248544701c
Mankey <79991410+Mankeyss@users.noreply.github.com>
2024-01-05 23:51:36 +0100

    Update script.js

--- files/script.js
+++ files/script.js
@@ -13,7 +13,7 @@ function calc() {
     var outPutUnit = select.options[select.selectedIndex].text
 
     if(outPutUnit === "Day(s)") {
-        outPutToMilliseconds = (document.querySelector('input').value)*60*60*60*1000;
+        outPutToMilliseconds = (document.querySelector('input').value)*24*60*60*1000;
     }
     if(outPutUnit === "Hour(s)") {
         outPutToMilliseconds = (document.querySelector('input').value)*60*60*1000;
@@ -29,7 +29,7 @@ function calc() {
     }
 
     if(outPutUnit2 === "Day(s)") {
-        result = outPutToMilliseconds/60/60/60/1000
+        result = outPutToMilliseconds/24/60/60/1000
     }
     if(outPutUnit2 === "Hour(s)") {
         result = outPutToMilliseconds/60/60/1000
